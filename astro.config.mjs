65cf35f31fae107d97e3b205997494d87bdae053
shnakamura <joaovtnakamura@gmail.com>
2025-02-27 21:17:27 -0300

    feat: workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
+import { WEBSITE_SERVER_PORT, WEBSITE_URL, WEBSITE_BASE } from "./src/scripts/constants.js";
+
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: WEBSITE_SERVER_PORT },
+  site: WEBSITE_URL,
   vite: {
     plugins: [tailwindcss()]
   }
--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -5,3 +5,7 @@ export const INSTAGRAM_URL = "https://www.instagram.com/xdjfuryoficial/";
 export const YOUTUBE_URL = "https://www.youtube.com/@djfuryoficial";
 
 export const SPOTIFY_ARTIST_ID = "6CHfctV0xTse0qz9LGlpoK";
+
+export const WEBSITE_SERVER_PORT = 3000;
+export const WEBSITE_URL = "https://shnakamura.github.io";
+export const WEBSITE_BASE = `http://localhost:${WEBSITE_SERVER_PORT}`;
