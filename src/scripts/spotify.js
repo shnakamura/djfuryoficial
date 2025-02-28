d8896e30c3bb9d6815a57bded3e04d16c882f358
shnakamura <joaovtnakamura@gmail.com>
2025-02-27 21:09:03 -0300

    feat: renames project

--- src/scripts/spotify.js
+++ src/scripts/spotify.js
@@ -16,24 +16,23 @@ export async function getSpotifyToken() {
     return data.access_token;
 }
 
-export async function getLatestSongs(id, amount) {
-    const token = await getSpotifyToken();
-  
-    const response = await fetch(
-      `https://api.spotify.com/v1/artists/${id}/top-tracks?market=US`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-  
-    const data = await response.json();
+export async function getLatestSongs(id, amount = 3) {
+  const token = await getSpotifyToken();
 
-  
-    return data.tracks.slice(0, amount).map((track) => ({
-      name: track.name,
-      artists: track.artists.map((artist) => artist.name).join(", "),
-      release_date: track.album.release_date,
-      image: track.album.images[0]?.url || "",
-      url: track.external_urls.spotify,
-    }));
+  const response = await fetch(
+  `https://api.spotify.com/v1/artists/${id}/top-tracks?market=US`,
+  {
+    headers: { Authorization: `Bearer ${token}` },
   }
+  );
+
+  const data = await response.json();
+
+  return data.tracks.slice(0, amount).map((track) => ({
+    name: track.name,
+    artists: track.artists.map((artist) => artist.name).join(", "),
+    release_date: track.album.release_date,
+    image: track.album.images[0]?.url || "",
+    url: track.external_urls.spotify,
+  }));
+}
